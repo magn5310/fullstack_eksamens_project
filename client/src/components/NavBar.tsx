b94c99c7725c102b43766f509200a3c2751f6da1
magn5310 <magnusmadsen2000@hotmail.com>
2025-06-04 15:49:55 +0200

    profile side lavet

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -9,7 +9,7 @@ interface NavBarProps {
 }
 
 function NavBar({}: NavBarProps) {
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(true);
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -32,7 +32,7 @@ function NavBar({}: NavBarProps) {
         </div>
         <div className="flex items-center gap-2">
           {login ? (
-            <Link href="/login" className="text-[#fffffe]">
+            <Link href="/profile" className="text-[#fffffe]">
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
               </svg>
